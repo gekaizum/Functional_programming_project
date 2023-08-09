2c5757d7693d7497c5a3a28c9ac9f7c0feddf6ac
ShakedBasa <128978272+ShakedBasa@users.noreply.github.com>
2023-08-09 22:40:44 +0300

    Add files via upload

---
+++ gui.erl
@@ -0,0 +1,194 @@
+-module(gui).
+-export([start/0]).
+-export([handle_click/2, start_sim/4]).
+-include_lib("wx/include/wx.hrl").
+-author("Shaked Basa").
+-define(x_cell_size,(40)).
+-define(y_cell_size,(40)).
+-define(x_leaf_size,(20)).
+-define(y_leaf_size,(20)).
+-define(refresh_time,(250)).
+
+start() ->
+	% Start the wxWidgets application
+	wx:new(),
+	% Create the main frame
+	Frame_1 = wxFrame:new(wx:null(), 1, "Life_and_Evolution_of_cells - Menu"),
+	% Create static labels
+	Label_1 = wxStaticText:new(Frame_1, ?wxID_ANY, "Number of Cells"),
+	Label_2 = wxStaticText:new(Frame_1, ?wxID_ANY, "World Size"),
+	% Create input text fields with initial values and sizes
+	Input_1 = wxTextCtrl:new(Frame_1, ?wxID_ANY,[{value, "50"}, {size, {150,50}}]),
+	Input_2 = wxTextCtrl:new(Frame_1, ?wxID_ANY,[{value, "800"}, {size, {150,50}}]),
+	% Create a font for the text fields
+	Font = wxFont:new(42,?wxFONTFAMILY_DEFAULT, ?wxFONTSTYLE_NORMAL, ?wxFONTWEIGHT_BOLD),
+	wxTextCtrl:setFont(Input_1, Font),
+	wxTextCtrl:setFont(Input_2, Font),
+	% Create a button labeled "Start"
+	Button = wxButton:new(Frame_1, ?wxID_ANY, [{label, "Start"}, {pos,{0, 64}}, {size, {150, 50}}]),
+	% Create a sizer to arrange the elements vertically
+	MainSizer = wxBoxSizer:new(?wxVERTICAL),
+	wxSizer:add(MainSizer, Label_1, [{flag, ?wxALIGN_CENTRE bor ?wxALL}, {border, 5}]),
+	wxSizer:add(MainSizer, Input_1, [{flag, ?wxEXPAND bor ?wxALL}, {border,5}]),
+	wxSizer:add(MainSizer, Label_2, [{flag, ?wxALIGN_CENTRE bor ?wxALL}, {border, 5}]),
+	wxSizer:add(MainSizer, Input_2, [{flag, ?wxEXPAND bor ?wxALL}, {border,5}]),
+	wxSizer:add(MainSizer, Button, [{flag, ?wxEXPAND bor ?wxALL}, {border,5}]),
+	wxWindow:setSizer(Frame_1, MainSizer),
+	wxSizer:setSizeHints(MainSizer, Frame_1),
+	% Connect the button click event to the handle_click function
+	wxButton:connect(Button, command_button_clicked, [{callback, fun handle_click/2}, {userData, #{input_1 => Input_1,input_2 => Input_2, env => wx:get_env()}}]),
+	% Show the main frame
+	wxFrame:show(Frame_1).
+
+%% Function to handle button click events
+handle_click(#wx{obj = Button, userData = #{input_1 := Input_1,input_2 := Input_2, env := Env}}, _Event) ->
+	% Set the environment
+	wx:set_env(Env),
+	% Get the label of the clicked button
+	Label = wxButton:getLabel(Button),
+	% Handle button label
+	case Label of
+
+		"Start" ->
+			% Disable input fields and change button label to "Stop"
+			wxTextCtrl:setEditable(Input_1, false),
+			wxTextCtrl:setEditable(Input_2, false),
+			wxButton:setLabel(Button, "Stop"),
+			% Spawn a new process to start the simulation	
+			spawn(?MODULE, start_sim, [Input_1, Input_2, Button, Env]);
+		
+		"Stop" ->
+			% Enable input fields and change button label to "Start"
+			wxTextCtrl:setEditable(Input_1, true),
+			wxTextCtrl:setEditable(Input_2, true),
+			wxButton:setLabel(Button, "Start")
+	end.
+
+
+start_sim(Input_1, Input_2, Button, Env) ->
+	
+	wx:set_env(Env),
+	N = list_to_integer(wxTextCtrl:getValue(Input_1)),
+	Frame_size = list_to_integer(wxTextCtrl:getValue(Input_2)),
+	% Create a new ETS named 'main_ets'
+	ets:new(main_ets,[public,named_table]),
+	
+	% Insert random cells into the 'main_ets' table	
+    	insert_cells(N,Frame_size),		
+
+	% Create a new frame (window)
+	Frame = wxFrame:new(wx:null(), 1, "Life_and_Evolution_of_cells",[{size,{Frame_size,Frame_size}}]),
+	
+	% Load and scale the cells images from file
+	General = wxImage:new("general.png"),
+	Generalc = wxImage:scale(General,?x_cell_size,?y_cell_size),
+    	BmpGeneral = wxBitmap:new(Generalc),
+  	wxImage:destroy(General),
+  	wxImage:destroy(Generalc),
+
+	Ceed = wxImage:new("ceed.png"),
+	Ceedc = wxImage:scale(Ceed,?x_cell_size,?y_cell_size),
+    	BmpCeed = wxBitmap:new(Ceedc),
+  	wxImage:destroy(Ceed),
+  	wxImage:destroy(Ceedc),
+
+	Leaf = wxImage:new("leaf.png"),
+	Leafc = wxImage:scale(Leaf,?x_leaf_size,?y_leaf_size),
+    	BmpLeaf = wxBitmap:new(Leafc),
+  	wxImage:destroy(Leaf),
+  	wxImage:destroy(Leafc),
+
+	Antena = wxImage:new("antena.png"),
+	Antenac = wxImage:scale(Antena,?x_cell_size,?y_cell_size),
+    	BmpAntena = wxBitmap:new(Antenac),
+  	wxImage:destroy(Antena),
+  	wxImage:destroy(Antenac),
+
+	Root = wxImage:new("root.png"),
+	Rootc = wxImage:scale(Root,?x_cell_size,?y_cell_size),
+    	BmpRoot = wxBitmap:new(Rootc),
+  	wxImage:destroy(Root),
+  	wxImage:destroy(Rootc),
+
+	% Print cells on the canvas based on the data in 'main_ets' table
+	print_cells(main_ets,ets:first(main_ets), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot),
+	
+	% Show the frame to display the canvas
+	wxFrame:show(Frame),
+	canvas_loop(Frame_size, Button, main_ets, Frame, BmpGeneral, BmpCeed, BmpLeaf, BmpAntena, BmpRoot, N, 50).
+	
+
+%% Function to continuously update and display the canvas
+canvas_loop(_Frame_size, _Button,_main_ets,Frame, _BmpGeneral, _BmpCeed, _BmpLeaf , _BmpAntena , _BmpRoot,_N,0) ->
+	ets:delete(main_ets),				
+	wxFrame:destroy(Frame),	
+	ok;	
+canvas_loop(Frame_size, Button,main_ets, Frame, BmpGeneral, BmpCeed, BmpLeaf, BmpAntena, BmpRoot, N, I) ->
+	case wxButton:getLabel(Button) of
+		"Stop" ->
+			% Clear the 'main_ets' table
+			ets:delete_all_objects(main_ets),
+			% Insert new random cells into the 'main_ets' table
+			insert_cells(N,Frame_size),
+			% Clear the canvas by destroying all children of the frame
+			wxWindow:destroyChildren(Frame),
+			% Print the updated cells on the canvas
+			print_cells(main_ets,ets:first(main_ets), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot),
+			% Refresh the canvas to display the changes
+			wxWindow:refresh(Frame),
+			% Introduce a delay for animation effect
+			timer:sleep(?refresh_time),
+			 % Recursive call to continue the loop
+			canvas_loop(Frame_size, Button, main_ets, Frame, BmpGeneral, BmpCeed, BmpLeaf, BmpAntena, BmpRoot, N, I-1);
+		"Start" ->
+			% Delete the 'main_ets' table
+			ets:delete(main_ets),				
+			wxFrame:destroy(Frame)
+	end.
+
+% Function to print cells on the canvas based on the data in 'main_ets' table
+% Input: Key (the current key in the 'main_ets' table)
+%        Frame (the frame where the cells are displayed)
+%        Bmp_x_ (the _x_ bitmap)
+%        _x_ = {General, Ceed, Leaf, Antena, Root}
+print_cells(main_ets, Key, Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot) ->
+	case ets:lookup(main_ets, Key) of
+		[] ->	
+			finished;
+		[{{X,Y},general}] ->
+			wxStaticBitmap:new(Frame,?wxID_ANY , BmpGeneral, [{pos,{X,Y}}]),
+			print_cells(main_ets, ets:next(main_ets,Key), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot);
+		[{{X,Y}, ceed}] ->
+			wxStaticBitmap:new(Frame, ?wxID_ANY, BmpCeed, [{pos,{X,Y}}]),
+			print_cells(main_ets, ets:next(main_ets,Key), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot);
+		[{{X,Y}, leaf}] ->
+			wxStaticBitmap:new(Frame, ?wxID_ANY, BmpLeaf, [{pos,{X,Y}}]),
+			print_cells(main_ets, ets:next(main_ets,Key), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot);
+		[{{X,Y}, antena}] ->
+			wxStaticBitmap:new(Frame, ?wxID_ANY, BmpAntena, [{pos,{X,Y}}]),
+			print_cells(main_ets, ets:next(main_ets,Key), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot);
+		[{{X,Y}, root}] ->
+			wxStaticBitmap:new(Frame, ?wxID_ANY, BmpRoot, [{pos,{X,Y}}]),
+			print_cells(main_ets, ets:next(main_ets,Key), Frame, BmpGeneral, BmpCeed, BmpLeaf , BmpAntena , BmpRoot)
+	end.
+
+% Function to insert random points (cells) into the 'main_ets' table
+% Input: N (number of points to insert)	
+insert_cells(0,_) -> 
+	ok;
+insert_cells(N,Frame_size) ->
+	% Insert a random blue cell and a random green cell into the 'main_ets' table
+	ets:insert(main_ets, {{rand:uniform(Frame_size),rand:uniform(Frame_size)}, general}),
+	ets:insert(main_ets, {{rand:uniform(Frame_size),rand:uniform(Frame_size)}, ceed}),
+	ets:insert(main_ets, {{rand:uniform(Frame_size),rand:uniform(Frame_size)}, leaf}),
+	ets:insert(main_ets, {{rand:uniform(Frame_size),rand:uniform(Frame_size)}, antena}),
+	ets:insert(main_ets, {{rand:uniform(Frame_size),rand:uniform(Frame_size)}, root}),
+
+	% Recursively insert (N-1) points
+	insert_cells(N - 1, Frame_size).
+
+
+
+
+
+
