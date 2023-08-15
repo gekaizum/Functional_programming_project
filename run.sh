f7bbe2074b55624a480da00ad1518c26e691d62d
ShakedBasa <128978272+ShakedBasa@users.noreply.github.com>
2023-08-15 12:01:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh csestudent@132.72.81.60
-	cd Desktop/Shaked_yevgeniy/
+ssh -T csestudent@132.72.81.60 << EOF
+	cd Desktop/Shaked_yevgeniy/ 
 	erl -setcookie skey -name node4@132.72.81.60
 	c(graphic_node).
 	c(main_logger).
@@ -10,9 +10,8 @@ ssh csestudent@132.72.81.60
 	c(cell_manager).
 	c(cell_funcs).
 	c(general_cell_funcs).
-	exit
-
-ssh csestudent@132.72.81.60
+EOF
+ssh -T csestudent@132.72.81.167 << EOF
 	cd Desktop/Shaked_yevgeniy/
 	erl -setcookie skey -name node3@132.72.81.167
 	c(graphic_node).
@@ -22,9 +21,8 @@ ssh csestudent@132.72.81.60
 	c(cell_manager).
 	c(cell_funcs).
 	c(general_cell_funcs).
-	exit
-
-ssh csestudent@132.72.81.60
+EOF
+ssh -T csestudent@132.72.81.224 << EOF
 	cd Desktop/Shaked_yevgeniy/
 	erl -setcookie skey -name node2@132.72.81.224
 	c(graphic_node).
@@ -34,9 +32,8 @@ ssh csestudent@132.72.81.60
 	c(cell_manager).
 	c(cell_funcs).
 	c(general_cell_funcs).
-	exit
-
-ssh csestudent@132.72.81.60 
+EOF
+ssh -T csestudent@132.72.80.185 << EOF
 	cd Desktop/Shaked_yevgeniy/
 	erl -setcookie skey -name node1@132.72.80.185
 	c(graphic_node).
@@ -46,9 +43,8 @@ ssh csestudent@132.72.81.60
 	c(cell_manager).
 	c(cell_funcs).
 	c(general_cell_funcs).
-	exit
-
-erl -setcookie skey -name graphic_node@132.72.81.85
+EOF
+erl -setcookie skey -name graphic_node@132.72.81.85 << EOF
 c(graphic_node).
 c(main_logger).
 c(general_node).
@@ -58,3 +54,4 @@ c(cell_funcs).
 c(general_cell_funcs).
 c(n_gui).
 n_gui:start().
+EOF
